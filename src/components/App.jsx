d86e8e8d16c761636e5f26e2fc2f20f374d5335f
caioguarnieri <92325326+caioguarnieri@users.noreply.github.com>
2022-12-18 01:06:20 +0000

    adiciona o header ao App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Signup from "../pages/auth/Signup";
 import Login from "../pages/auth/Login";
+import { Header } from "./Header";
 
 import { AuthContextComponent } from "../contexts/authContext";
 
 function App() {
   return (
     <AuthContextComponent>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
     </AuthContextComponent>
   );
 }
